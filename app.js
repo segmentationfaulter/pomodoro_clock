5cc8f5056358aa688beb73df10a2d41054ee12f9
segmentationfaulter <muhammadsackib@gmail.com>
2015-09-12 09:37:05 +0500

    do basic timer triggered by start button

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
-  var targetTime, intervalID;
+  var targetTime, intervalID, secondsLeft;
+
+  // displays the secondsLeft before clicking the start button
+  targetTime = Date.now() + (1 * 60 * 1000);
+  secondsLeft = Math.ceil((targetTime - Date.now()) / 1000);
+  $('h1').text(secondsLeft);
+
   $('button').on('click', function() {
+    // re-calculates the secondsLeft after clicking the start button
+    // we can't rely on the values calculated previously since
+    // htere is a delay involved in calculating the above value of secondsLeft
+    // and clicking the start button
+
     targetTime = Date.now() + (1 * 60 * 1000);
+    secondsLeft = Math.ceil((targetTime - Date.now()) / 1000);
     intervalID = setInterval(timer, 1000);
   });
   // timer callback for setInterval function
   function timer() {
-    var secondsLeft = Math.ceil((targetTime - Date.now()) / 1000);
+    secondsLeft = Math.ceil((targetTime - Date.now()) / 1000); // fresh value of secondsLeft is calculated after every second
     $('h1').text(secondsLeft);
     if (secondsLeft <= 0)
       clearInterval(intervalID);
