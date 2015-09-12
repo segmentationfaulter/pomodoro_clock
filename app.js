bc5549b68d200fc9b3aed2d250821b3174a9a693
segmentationfaulter <muhammadsackib@gmail.com>
2015-09-12 10:45:23 +0500

    do the very basic layout

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
   // displays the secondsLeft before clicking the start button
   targetTime = Date.now() + (1 * 60 * 1000);
   secondsLeft = Math.ceil((targetTime - Date.now()) / 1000);
-  $('h1').text(secondsLeft);
+  $('#down-counter').text(secondsLeft);
 
-  $('button').on('click', function() {
+  $('.timer').on('click', 'button', function() {
     // re-calculates the secondsLeft after clicking the start button
     // we can't rely on the values calculated previously since
     // htere is a delay involved in calculating the above value of secondsLeft
@@ -19,7 +19,7 @@ $(document).ready(function() {
   // timer callback for setInterval function
   function timer() {
     secondsLeft = Math.ceil((targetTime - Date.now()) / 1000); // fresh value of secondsLeft is calculated after every second
-    $('h1').text(secondsLeft);
+    $('#down-counter').text(secondsLeft);
     if (secondsLeft <= 0)
       clearInterval(intervalID);
   }
