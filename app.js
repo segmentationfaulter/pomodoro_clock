7239690f36a60c365e8489155bb3c66ebffc7cdf
segmentationfaulter <muhammadsackib@gmail.com>
2015-09-14 06:34:51 +0500

    refactor the timer function so can be reused-part I

--- app.js
+++ app.js
@@ -88,47 +88,50 @@ $(document).ready(function() {
 
   displayTimer('#session-length');
 
-  var intervalID, targetTime;
-  $('#timer-controls').on('click', '#start-work', function() {
+  function timer(sessionType) {
+    var intervalID, targetTime;
+    $('#timer-controls').on('click', '#start-work', function() {
 
-    /**
-     * look what will happen if the three lines of code below are not there.
-     * when a user will click the start button first tiem, upon calling setInterval,
-     * a unique intervalID (say it is 1) will be assigned against that call which will be used to end the repeated call to callback.
-     * suppose a user clicks the start button second time, the call to setInterval will be made again
-     * and value of intervalID will be overwritten and will be changed to 2. Now you cannot end
-     * the repeated calls to callback function which were initiated against intervalID of 1
-     * and the callback function will be called infinitely.
-     */
-    if (intervalID) {
-      clearInterval(intervalID);
+      /**
+       * look what will happen if the three lines of code below are not there.
+       * when a user will click the start button first tiem, upon calling setInterval,
+       * a unique intervalID (say it is 1) will be assigned against that call which will be used to end the repeated call to callback.
+       * suppose a user clicks the start button second time, the call to setInterval will be made again
+       * and value of intervalID will be overwritten and will be changed to 2. Now you cannot end
+       * the repeated calls to callback function which were initiated against intervalID of 1
+       * and the callback function will be called infinitely.
+       */
+      if (intervalID) {
+        clearInterval(intervalID);
+      }
+
+      // disable the unwanted buttons once timer is started
+      $(this).attr('disabled', '');
+      $('#start-break').attr('disabled', '');
+      $('#session-up').attr('disabled', '');
+      $('#session-down').attr('disabled', '');
+
+      /**
+       * re-calculates the secondsLeft after clicking the start button
+       * we can't rely on the values calculated previously since
+       * there is a delay involved in calculating the above value of secondsLeft
+       * and clicking the start button
+       */
+
+      var sessionLength = +$(sessionType).text();
+      targetTime = Date.now() + (sessionLength * 60 * 1000);
+      intervalID = setInterval(cb, 1000);
+    });
+
+    // timer callback for setInterval function
+    function cb() {
+
+      // fresh value of secondsLeft is calculated after every second
+      var secondsLeft = Math.ceil((targetTime - Date.now()) / 1000);
+      $('#down-counter').text(timeLeft(secondsLeft));
+      if (secondsLeft <= 0)
+        clearInterval(intervalID);
     }
-
-    // disable the unwanted buttons once timer is started
-    $(this).attr('disabled', '');
-    $('#start-break').attr('disabled', '');
-    $('#session-up').attr('disabled', '');
-    $('#session-down').attr('disabled', '');
-
-    /**
-     * re-calculates the secondsLeft after clicking the start button
-     * we can't rely on the values calculated previously since
-     * there is a delay involved in calculating the above value of secondsLeft
-     * and clicking the start button
-     */
-
-    var sessionLength = +$('#session-length').text();
-    targetTime = Date.now() + (sessionLength * 60 * 1000);
-    intervalID = setInterval(timer, 1000);
-  });
-
-  // timer callback for setInterval function
-  function timer() {
-
-    // fresh value of secondsLeft is calculated after every second
-    var secondsLeft = Math.ceil((targetTime - Date.now()) / 1000);
-    $('#down-counter').text(timeLeft(secondsLeft));
-    if (secondsLeft <= 0)
-      clearInterval(intervalID);
   }
+  timer('#session-length');
 });
